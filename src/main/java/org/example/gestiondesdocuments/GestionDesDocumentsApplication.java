d0c9dd5f32ec7379ca5de3a40e8105ef8c51e8ab
Adil Ait Elhoucine <adilaitelhoucine1@gmail.com>
2025-11-17 11:33:03 +0100

    first commit

---
+++ src/main/java/org/example/gestiondesdocuments/GestionDesDocumentsApplication.java
@@ -0,0 +1,13 @@
+package org.example.gestiondesdocuments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesDocumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesDocumentsApplication.class, args);
+	}
+
+}
